c1d49441e2d0fd75b89e4cc43cc904bd3946ca64
duyen phan <ptmduyenkhtn@gmail.com>
2023-12-12 00:34:40 +0700

    prevent modal from scrolling

--- src/app.vue
+++ src/app.vue
@@ -4,3 +4,17 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+import useModalStore from '~/stores/modal.store'
+
+const modalStore = useModalStore()
+useHead({
+  bodyAttrs: {
+    class: computed(() => {
+      if (modalStore.list.length) return 'modal-open';
+      return '';
+    }),
+  },
+})
+</script>
